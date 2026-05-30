e5f489b30e39346564e27870c942f1c5f5523b26
iqbolsharifov <148532950+iqbolsharifov@users.noreply.github.com>
2026-05-31 00:07:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function sendToTelegram(messageText) {
 
 // РАМЗҲОИ ВОРИДШАВӢ
 const USERS = {
-    "100403": { name: "Ширинмоҳ", role: "shirin" },
+    "100403": { name: "Ширинмоҳ", role: "shirinak" },
     "160205": { name: "Иқбол", role: "iqbol" }
 };
 
